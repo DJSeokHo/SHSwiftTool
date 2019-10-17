d3d302fb2d78410090342063038e04c12b5f3fc4
seokho <djseokho@vip.qq.com>
2019-10-17 18:20:21 +0900

    add BasicUIViewController

---
+++ SHSwiftTool/Template/BasicUIViewController/BasicUIViewController.swift
@@ -0,0 +1,58 @@
+//
+//  BasicUIViewController.swift
+//  SHSwiftTool
+//
+//  Created by Seok Ho on 2019/10/17.
+//  Copyright © 2019 SWein. All rights reserved.
+//
+
+import UIKit
+
+class BasicUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
